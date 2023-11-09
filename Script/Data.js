11c08b08ef66bbeae235597eee28dbe33e372e2f
Daxnekkitsune <boterolondonocamilo@gmail.com>
2023-11-09 16:23:22 -0500

    Commit inicial

---
+++ Script/Data.js
@@ -0,0 +1,29 @@
+export const  = [
+    {
+        id:"01",
+        imagen:""
+    },
+    {
+        id:"02",
+        imagen:""
+    },
+    {
+        id:"03",
+        imagen:""
+    }
+    ,
+    {
+        id:"04",
+        imagen:""
+    }
+    ,
+    {
+        id:"05",
+        imagen:""
+    }
+    ,
+    {
+        id:"06",
+        imagen:""
+    }
+]
